2fe355258014695e88f98fe2a6a6aaca1868ab11
BECKHAMW3233 <156467180+BECKHAMW3233@users.noreply.github.com>
2024-02-09 15:29:31 -0500

    Add files via upload

---
+++ P1LAB1_BeckhamWilliam.py
@@ -0,0 +1,17 @@
+ # William Beckham
+
+ # 02-09-2024
+
+ # P1LAB1
+
+ # This assignment teaches the student how to create a Python program using IDLE. It shows how to get input from the user and display output.
+
+
+
+
+first="William"
+x = "Welcome"
+y = "to"
+z = "zyBooks!"
+print(first)
+print(x, y, z)
