74fe8fe29ecdce89ae26cf88bb866d854e113f15
William Beckham <156467180+BECKHAMW3233@users.noreply.github.com>
2024-02-09 21:29:54 -0500

    Add files via upload

---
+++ P1HW1_BeckhamWilliam.py
@@ -0,0 +1,16 @@
+ # William Beckham
+ # 02-09-2024
+ # P1HW1
+ # Assignment tests student's knowledge of how to write code that collects information from user, processes information collected and display results to user.
+
+user_num = int(input('Enter integer:\n')) 
+print("You entered:", user_num)
+print(user_num, "sqaured is", user_num ** 2)
+print("And", user_num, "cubed is", user_num ** 3, "!!")
+
+
+user_num2 = int(input('Enter another integer:\n'))
+print("You entered:", user_num2)
+v1 = 4
+print("4 +", user_num2, "is", user_num2 + v1)
+print("4 *", user_num2, "is", user_num2 * v1)
