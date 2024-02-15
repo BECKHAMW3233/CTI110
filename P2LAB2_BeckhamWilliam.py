24e27941ab1c90e36b6d50b588af93f25d80f673
William Beckham <156467180+BECKHAMW3233@users.noreply.github.com>
2024-02-15 11:36:25 -0500

    Add files via upload

---
+++ P2LAB2_BeckhamWilliam.py
@@ -0,0 +1,16 @@
+ # William Beckham
+ # 02-15-2024
+ # P2Lab2
+ # Assignment tests student's knowledge of how to write code that displays information to users
+
+
+num1 = float(input('enter number here:\n'))
+num2 = float(input('enter number here:\n'))
+num3 = float(input('enter number here:\n'))
+num4 = float(input('enter number here:\n'))
+
+your_value1 = num1 * num2 * num3 * num4 
+your_value2 = ((num1 + num2 + num3 + num4) /4)
+
+print(f'{your_value1:.0f} {your_value2:.0f}')
+print(f'{your_value1:.3f} {your_value2:.3f}')
