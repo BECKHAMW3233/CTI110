f631f54327f4415d41d7cd184fdf83273ae3cac5
William Beckham <156467180+BECKHAMW3233@users.noreply.github.com>
2024-02-15 11:08:46 -0500

    Add files via upload

---
+++ P2LAB1_BeckhamWilliam.py
@@ -0,0 +1,15 @@
+ # William Beckham
+ # 02-15-2024
+ # P2LAB1
+ # Assignment tests student's knowledge of how to write code that performs mathematical calculations and displays information to users
+
+
+
+miles_per_gallon=float(input('miles per gallon\n'))
+dollars_per_gallon=float(input('dollars per gallon\n'))
+print('\n')
+dollars_per_mile = dollars_per_gallon/miles_per_gallon
+your_value1 = 20 * dollars_per_mile
+your_value2 = 75 * dollars_per_mile
+your_value3 = 500 * dollars_per_mile
+print(f'{your_value1:.2f} {your_value2:.2f} {your_value3:.2f}')
