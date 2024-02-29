91b319b4813d9df413c428f07dc4a6b54696c9cc
William Beckham <156467180+BECKHAMW3233@users.noreply.github.com>
2024-02-29 13:43:36 -0500

    Add files via upload

--- P4LAB1_BeckhamWilliam.py
+++ P4LAB1_BeckhamWilliam.py
@@ -1,5 +1,6 @@
 # William Beckham
 # P4LAB1
+# 02-29-2024
 # Write a turtle graphics programs that draws a triangle and a square using loops.
 
 
@@ -16,7 +17,7 @@ def draw_triangle(t):
 def draw_square(t):
     for _ in range(4):
         t.right(90)
-        t.forward(100)
+        t.forward(200)
 
 def main():
     # Create screen and turtle objects
@@ -25,7 +26,7 @@ def main():
 
     # Set screen size and background color
     s.setup(800, 800)  
-    s.bgcolor("black")
+    s.bgcolor("grey")
 
     # Set turtle properties
     t.pensize(5)  # Set line thickness
@@ -42,7 +43,7 @@ def main():
     # Draw the second triangle
     draw_triangle(t)
 
-    t.forward(50)
+    t.forward(100)
 
     # Draw a square box centered under the triangles
     draw_square(t)
@@ -53,31 +54,30 @@ def main():
     # Draw a simple face on kitty 
     t.pencolor("black")
     t.right(90)
-    t.forward(25)
+    t.forward(50)
     t.right(90)
-    t.forward(20)
+    t.forward(50)
     t.pendown()
     t.forward(3)
     t.penup()
-    t.forward(42)
+    t.forward(100)
     t.pendown()
     t.forward(3)
     t.penup()
     t.left(90)
-    t.forward(25)
+    t.forward(50)
     t.left(90)
-    t.forward(25)
+    t.forward(50)
     t.pendown()
     t.forward(3)
     t.penup()
     t.right(90)
-    t.forward(25)
+    t.forward(50)
     t.right(90)
     t.forward(15)
     t.pendown()
-    t.back(24)
-    t.penup()
-
+    t.back(50)
+    t.penup()  
     print("meow I'm a kitty meow")
 
     # Keep the window open
@@ -85,3 +85,4 @@ def main():
 
 if __name__ == '__main__':
     main()
+    
